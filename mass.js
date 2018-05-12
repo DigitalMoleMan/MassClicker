50e57637c094e411ee95713e075a953d424a918e
DigitalMoleMan <DigitalMoleGuy@gmail.com>
2018-05-12 21:22:52 +0200

    Slight cleanup

--- mass.js
+++ mass.js
@@ -1,18 +1,20 @@
 /*
 Mass Clicker - script
 */
-var resAmount = 0;
-var cps = 0;
 
-var shopOpen = false;
+//money
+var resAmount = 0;
+
 /*Worker Units*/
 var workerAmount = 0;
 var workerBuyCost = 10;
 
+//adds money on click
 function addRes() {
     resAmount += 1;
 }
 
+//adds a worker and increases the worker price uppon passing a money check
 function buyWorker() {
     if (resAmount >= workerBuyCost) {
         resAmount -= workerBuyCost;
@@ -20,27 +22,18 @@ function buyWorker() {
         workerBuyCost += (workerAmount * workerAmount);
     }
 }
-
+//timer for how often workers produce money
 var tickSpeed = setInterval(gameTick, 1000);
 
 function gameTick() {
     resAmount += workerAmount;
 }
 
-function upgradeWorker() {
-    if (workers >= upgradeCost) {
-        workers -= upgradeCost;        
-    }
-}
-
+//updates all values seen on screen
 var updateSpeed = setInterval(updateValues, 10);
 
 function updateValues(){
     document.getElementById("res_amount").innerHTML ="$ " + resAmount;
     document.getElementById("worker_price").innerHTML ="$ " + workerBuyCost;
     document.getElementById("worker_amount").innerHTML = workerAmount;
-}
-
-function dev() {
-    res = res + 999999;
 }
