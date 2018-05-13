e6a25dbee9f8290fd1ee1d145e5dafd094526ed1
DigitalMoleMan <DigitalMoleGuy@gmail.com>
2018-05-13 02:47:19 +0200

    added banker and smoother numbers

--- mass.js
+++ mass.js
@@ -9,9 +9,14 @@ var resAmount = 0;
 var workerAmount = 0;
 var workerBuyCost = 10;
 
+/*Banker Units (Clicker Upgrades)*/
+var clickerUpgradeAmount = 0;
+var clickerMultiplier = 1;
+var clickerUpgradeCost = 1000;
+
 //adds money on click
 function addRes() {
-    resAmount += 1;
+    resAmount += (1 * clickerMultiplier);
 }
 
 //adds a worker and increases the worker price uppon passing a money check
@@ -19,21 +24,36 @@ function buyWorker() {
     if (resAmount >= workerBuyCost) {
         resAmount -= workerBuyCost;
         workerAmount += 1;
-        workerBuyCost += (workerAmount * workerAmount);
+        workerBuyCost += (workerAmount * 2);
+    }
+}
+
+function buyClickerUpgrade() {
+    if (resAmount >= clickerUpgradeCost) {
+        resAmount -= clickerUpgradeCost;
+        clickerUpgradeAmount += 1;
+        clickerMultiplier *= 2;
+        clickerUpgradeCost *= clickerMultiplier;
     }
 }
 //timer for how often workers produce money
-var tickSpeed = setInterval(gameTick, 1000);
+setInterval(gameTick, 10);
 
 function gameTick() {
-    resAmount += workerAmount;
+    resAmount += ((workerAmount * clickerMultiplier) / 100);
 }
 
 //updates all values seen on screen
 var updateSpeed = setInterval(updateValues, 10);
 
 function updateValues(){
-    document.getElementById("res_amount").innerHTML ="$ " + resAmount;
+    document.getElementById("res_amount").innerHTML ="$ " + Math.round(resAmount);
     document.getElementById("worker_price").innerHTML ="$ " + workerBuyCost;
     document.getElementById("worker_amount").innerHTML = workerAmount;
+    document.getElementById("clicker_upgrade_amount").innerHTML = clickerUpgradeAmount;
+    document.getElementById("clicker_upgrade_price").innerHTML ="$ " + clickerUpgradeCost;
+}
+
+function dev() {
+    resAmount = 100000;
 }
